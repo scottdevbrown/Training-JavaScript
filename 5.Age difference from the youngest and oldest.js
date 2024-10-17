3a32f471b417d1e69fb0747beb341b597c282e4c
nakamoto <n.haruki316@gmail.com>
2024-10-17 09:36:57 -0700

    feat: add function for getting min, max age

--- 5.Age difference from the youngest and oldest.js
+++ 5.Age difference from the youngest and oldest.js
@@ -9,3 +9,21 @@
   ];
    Output: [13, 67, 54]
 */
+const inputs = [
+  { name: "John", age: 13},
+  { name: "Mark", age: 56},
+  { name: "Rachel", age: 45},
+  { name: "Nate", age: 67},
+  { name: "Jennifer", age: 65}
+];
+
+const values = [1,3,4,5,6]
+
+const extractAge = (inputs) =>{
+  maxAge = inputs.map(input => input.age).reduce((prev, curr)=>Math.max(prev, curr))
+  minAge = inputs.map(input => input.age).reduce((prev, curr)=>Math.min(prev, curr))
+  diffAge = maxAge - minAge
+  return {maxAge, minAge, diffAge}
+}
+
+console.log(extractAge(inputs))
