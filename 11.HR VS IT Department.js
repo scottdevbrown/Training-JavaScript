f6570088b1085f865ca473bd78f81659d9622132
nakamoto <n.haruki316@gmail.com>
2024-10-20 23:18:27 +0900

    feat:add function of getting high salary employee

--- 11.HR VS IT Department.js
+++ 11.HR VS IT Department.js
@@ -17,3 +17,42 @@ of objects containing only the departments that have an average salary above 650
   { department: 'HR', average: 71666 }
  ]
 */
+
+
+employees = [
+  { name: "John", salary: 50000, department: "IT" },
+  { name: "Jane", salary: 60000, department: "HR" },
+  { name: "Bob", salary: 55000, department: "IT" },
+  { name: "Sophie", salary: 75000, department: "HR" },
+  { name: "Mike", salary: 65000, department: "IT" },
+  { name: "Emily", salary: 80000, department: "HR" },
+  { name: "David", salary: 70000, department: "IT" },
+ ];
+
+const greaterThan = 65000
+
+  const getHighSalary = (employees, greaterThan) => {
+    const departmentArray = {}
+    employees.map(employee => {
+      if (!departmentArray[employee.department]) {
+        departmentArray[employee.department] = [employee.salary]
+      } else {
+        departmentArray[employee.department].push(employee.salary)
+      }      
+    })
+
+    averageArray = Object.keys(departmentArray).map(department => {
+      averageItem = {}
+      averageItem["department"] = department
+      if (departmentArray[department].length) {
+        averageItem["average"] = Math.floor(departmentArray[department].reduce((a, b) => a + b) / departmentArray[department].length)
+      } else {
+        averageItem["average"] = 0
+      }      
+      return averageItem
+    })
+
+    return averageArray.filter(averageItem => averageItem.average > greaterThan)     
+  }
+    
+  console.log(getHighSalary(employees, 65000))
