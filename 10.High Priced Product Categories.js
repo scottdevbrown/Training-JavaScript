2bb5af99bb9b6573cd53d3dfc54be6eef6896de9
Lam <lam.t.n.quang@gmail.com>
2024-10-20 11:49:18 -1200

    feat: the solution of problem10

--- 10.High Priced Product Categories.js
+++ 10.High Priced Product Categories.js
@@ -18,3 +18,33 @@ return an array of objects containing only the categories that have an average p
   { category: 'Electronics', average: 55 }
   ]
 */
+const products = [
+  { name: "Product 1", price: 20, category: "Electronics" },
+  { name: "Product 2", price: 30, category: "Clothes" },
+  { name: "Product 3", price: 40, category: "Electronics" },
+  { name: "Product 4", price: 50, category: "Clothes" },
+  { name: "Product 5", price: 60, category: "Clothes" },
+  { name: "Product 6", price: 70, category: "Electronics" },
+  { name: "Product 7", price: 80, category: "Clothes" },
+  { name: "Product 8", price: 90, category: "Electronics" },
+  ];
+const avearageFilter = (products) => {
+  let priceArrayByCategory = [];
+  let averageValueArrayByCategory = [];
+  products.map(product => {
+    if (priceArrayByCategory[product.category]) 
+      priceArrayByCategory[product.category].push(product.price);
+    else priceArrayByCategory[product.category] = [product.price];
+  })
+  let categoryArray = Object.keys(priceArrayByCategory) // return key words
+  categoryArray.map(categoryName => {
+    let avergageByCategory = { category: '', average: 0 };
+    avergageByCategory.average = priceArrayByCategory[categoryName].reduce((sum, value) => sum + value)/priceArrayByCategory[categoryName].length;
+    avergageByCategory.category = categoryName;
+    averageValueArrayByCategory.push(avergageByCategory);
+  })
+  return averageValueArrayByCategory.filter(category => category.average > 50);
+}
+console.log(avearageFilter(products));
+
+
