472ee11de8f6c62b0c10d4a2b4cc590822dff218
Lam <lam.t.n.quang@gmail.com>
2024-10-20 05:09:27 -1200

    feat: solution of problem5

--- 8.Count elements in array of arrays.js
+++ 8.Count elements in array of arrays.js
@@ -1,4 +1,4 @@
-/* Count the occurrences of distinct elements in the given 2D array. The given input is an array, the elements of which are arrays of strings. 
+/* Count the occurrences of distinct elements in the given 2D array. The given twoOrderArray is an array, the elements of which are arrays of strings. 
 The result is an object whose property names are the values from the arrays and their value is the number of their occurrences.
   Sample I/O:
    Input:  const input = [
@@ -9,3 +9,22 @@ The result is an object whose property names are the values from the arrays and 
 
    Output:  { a: 1, b: 1, c: 2, d: 2, f: 2, g: 1}
   */
+   const twoOrderArray = [
+    ["a", "b", "c"],
+    ["c", "d", "f"],
+    ["d", "f", "g"],
+ ];
+   const letterCount = (twoOrderArray) => {
+    const result = {}; 
+    twoOrderArray.forEach(oneOrderArray => {
+        oneOrderArray.forEach(oneLetter => {
+            if (result[oneLetter]) {
+                result[oneLetter] += 1;
+            } else {
+                result[oneLetter] = 1;
+            }
+        });
+    });
+    return result;
+   }
+   console.log(letterCount(twoOrderArray));
