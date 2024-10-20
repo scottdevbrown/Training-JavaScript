53a23a6cbe8334ae7b8abb8ae44fb7dc5ee58211
nakamoto <n.haruki316@gmail.com>
2024-10-21 04:16:47 +0900

    add func spike length

--- 13.Longeset spike length.js
+++ 13.Longeset spike length.js
@@ -16,3 +16,32 @@ Write an efficient algorithm for the following assumptions:
 N is an integer within the range [1..100,000]:
 each element of array A is an integer within the range [1..1,000,000]
 */
+
+
+const numberArray = [2, 3, 3, 2, 2, 2, 1]
+
+const solution = (numberArray) => {
+    sortArray = numberArray.sort((a, b) => a - b)
+    count = 1
+    repeat = 0
+    sortArray.reduce((prev, curr) => {        
+        if ((prev - curr) < 0) {
+            count++
+            repeat = 0
+        } else {
+            if(!repeat) {
+                count++
+            }
+            repeat++                        
+        }
+        return curr        
+    })
+    if (sortArray[sortArray.length - 1] === sortArray[sortArray.length - 2]) {
+        count--
+    }
+    return count
+}
+
+console.log(solution(numberArray))
+
+
