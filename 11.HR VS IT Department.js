b6d78a268ad2fadfca6715ae0f338db0246aec64
Lam <lam.t.n.quang@gmail.com>
2024-10-20 12:36:37 -1200

    feat: the solution of problem 11

--- 11.HR VS IT Department.js
+++ 11.HR VS IT Department.js
@@ -17,3 +17,33 @@ of objects containing only the departments that have an average salary above 650
   { department: 'HR', average: 71666 }
  ]
 */
+const products = [
+  { name: "John", salary: 50000, department: "IT" },
+  { name: "Jane", salary: 60000, department: "HR" },
+  { name: "Bob", salary: 55000, department: "IT" },
+  { name: "Sophie", salary: 75000, department: "HR" },
+  { name: "Mike", salary: 65000, department: "IT" },
+  { name: "Emily", salary: 80000, department: "HR" },
+  { name: "David", salary: 70000, department: "IT" },
+ ];
+
+const avearageFilter = (products) => {
+  let salaryArrayByCategory = [];
+  let averageValueArrayByCategory = [];
+  products.map(product => {
+    if (salaryArrayByCategory[product.department]) 
+      salaryArrayByCategory[product.department].push(product.salary);
+    else salaryArrayByCategory[product.department] = [product.salary];
+  })
+  let categoryArray = Object.keys(salaryArrayByCategory) // return key words
+  categoryArray.map(departmentName => {
+    let avergageByCategory = { department: '', average: 0 };
+    if (salaryArrayByCategory[departmentName].length == 0) 
+      console.log('Warning: There exists no products');
+    avergageByCategory.average = Math.floor(salaryArrayByCategory[departmentName].reduce((sum, value) => sum + value)/salaryArrayByCategory[departmentName].length);
+    avergageByCategory.department = departmentName;
+    averageValueArrayByCategory.push(avergageByCategory);
+  })
+  return averageValueArrayByCategory.filter(department => department.average > 65000);
+}
+console.log(avearageFilter(products));
